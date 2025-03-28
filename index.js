e6d9db2a7faa4725ba87f5128f0e8a212c951334
ABHISHEK KUMAR <130243689+ABHISHEK22KGP@users.noreply.github.com>
2025-03-28 22:39:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
+// getting express
 const express = require('express')
 const app = express()
+
+// getting third party middlware
 const morgan = require('morgan')
 app.use(morgan('dev'))
+
+// express cant read post data so to help it 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+// helps to add css and javascript 
 app.use(express.static('public'))
+
+// helps to add html/ejs file 
 app.set("view engine",'ejs')
-app.get('/',(req,res)=>{
-    res.send('Welcome !')
+
+
+// Database work
+const mongoose = require('mongoose');
+const connection = mongoose.connect('mongodb+srv://ABHISHEK22KGP2026:BHI9Jdk1zO3juE4U@cluster0.srcbnno.mongodb.net/1st_backend').then(()=>{
+    console.log("Connected To DataBase")
 })
+const userSchema = new mongoose.Schema({
+    Email:String,
+    Password:String
+})
+const userMOdel = mongoose.model('user',userSchema);
+
+
+
+
+// req to server 
+app.get('/',(req,res)=>{
+    res.send('Welcome!');
+})
+
+// post to server 
+// app.post('/get-form-data',async (req,res)=>{
+//     console.log(req.body);
+//     const {Email,password} = req.body;
+//     const newUser = await userMOdel.create({
+//         Email:Email,
+//         Password:password
+//     })
+//     res.send(newUser);
+// })
+
+// app.get('/get-users',(req,res)=>{
+//     userMOdel.find().then((users)=>{
+//         res.send(users);
+//     })
+// });
+// starting the server 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
